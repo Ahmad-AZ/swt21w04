dd4887a6457fbe8c340fd7499d2a9d075268af5c
georgm.kunze@gmail.com <georgm.kunze@gmail.com>
2021-11-03 13:53:25 +0100

    startet application prototype base (login/out works, no menus yet)

---
+++ src/main/java/prototype/Application.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package prototype;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	static class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/login").setViewName("login");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.csrf().disable();
+
+			http.authorizeRequests().anyRequest().permitAll().and()
+					.formLogin().and()
+					.logout().logoutSuccessUrl("/").clearAuthentication(true);
+		}
+
+		@Bean
+		@Override
+		public UserDetailsService userDetailsService() {
+			UserDetails admin =
+					User.withDefaultPasswordEncoder()
+							.username("admin")
+							.password("adminpw")
+							.roles("USER", "ADMIN")
+							.build();
+
+			UserDetails test =
+					User.withDefaultPasswordEncoder()
+							.username("test")
+							.password("testpw")
+							.roles("USER")
+							.build();
+
+			return new InMemoryUserDetailsManager(admin, test);
+		}
+	}
+}
