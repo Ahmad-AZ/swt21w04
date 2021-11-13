76dad13a0a4d90abc61bfb8d8e1a97c260eeffc3
Adrian <Adrian.Scholze@mailbox.tu-dresden.de>
2021-11-13 09:18:19 +0100

    create new festival with name works

--- src/main/java/prototype/festival/FestivalController.java
+++ src/main/java/prototype/festival/FestivalController.java
@@ -2,7 +2,10 @@ package prototype.festival;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FestivalController {
@@ -26,7 +29,40 @@ public class FestivalController {
 //
 //		return "detail";
 //	}
-//	
+	@PostMapping("/newFestival")
+	String registerNew(@Validated NewFestivalForm form, Errors result) {
+		
+//		Streamable<F> customers = customerManagement.findAll();
+//		for (Customer customer : customers) {
+//			if (form.getName().equals(customer.getUserAccount().getUsername())) {
+//				result.rejectValue("name", null, "Benutzername bereits vergeben");
+//			}
+//		}
+
+//		if (!form.getPassword().equals(form.getPasswordReputation())) {
+//			result.rejectValue("passwordReputation", null, "Passwörter stimmen nicht überein.");
+//
+//		}
+
+		if (result.hasErrors()) {
+			return "register";
+		}
+
+		// create Festival if no error appears
+		festivalManagement.createFestival(form);
+
+		return "redirect:/festivalOverview";
+	}
+	
+	
+	
+	// gives NewFestivalForm to fill out
+	@GetMapping("/newFestival")
+	String register(Model model, NewFestivalForm form) {
+		return "newFestival";
+	}
+	
+	// shows Festival Overview
 	@GetMapping("/festivalOverview")
 	public String festivals(Model model) {
 
--- src/main/java/prototype/festival/FestivalInitializer.java
+++ src/main/java/prototype/festival/FestivalInitializer.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-@Order(10)
+@Order(10)  
 public class FestivalInitializer implements DataInitializer {
 
 	private static final Logger LOG = (Logger) LoggerFactory.getLogger(FestivalInitializer.class);
