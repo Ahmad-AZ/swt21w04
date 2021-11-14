4a61ece33c8435e5c06b8ae1d3433a49e734694e
georgm.kunze@gmail.com <georgm.kunze@gmail.com>
2021-11-14 18:02:55 +0100

    created user management page

---
+++ src/main/java/prototype/staff/RemoveStaffForm.java
@@ -0,0 +1,16 @@
+package prototype.staff;
+
+import javax.validation.constraints.NotEmpty;
+
+public class RemoveStaffForm {
+	@NotEmpty(message = "{RemoveStaffForm.id.NotEmpty}")
+	Long id;
+
+	public RemoveStaffForm(Long id) {
+		this.id = id;
+	}
+
+	Long getId() {
+		return id;
+	}
+}
