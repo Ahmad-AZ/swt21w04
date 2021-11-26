d588c89a51369ed969b2d823e06f82a7ffa2a33d
Adrian <Adrian.Scholze@mailbox.tu-dresden.de>
2021-11-26 13:54:42 +0100

    add image/groundview to location works sometimes

--- src/main/java/festivalmanager/location/LocationController.java
+++ src/main/java/festivalmanager/location/LocationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import festivalmanager.staff.Person;
@@ -74,6 +75,9 @@ public class LocationController {
 //
 //		}
 
+		System.out.println(form.getImage());
+		System.out.println(form.getGroundView());
+
 		if (result.hasErrors()) {
 			return "newLocation";
 		}
@@ -83,7 +87,6 @@ public class LocationController {
 
 		return "redirect:/locations";
 	}
-	
 	
 	
 	// gives NewLocationForm to fill out
