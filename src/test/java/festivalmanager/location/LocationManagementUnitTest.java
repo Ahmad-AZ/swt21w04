9552ffa2338a5777cbed50207aaf2887233b8ae1
Adrian <Adrian.Scholze@mailbox.tu-dresden.de>
2021-11-28 23:35:05 +0100

    add LocationManagement Tests

--- src/test/java/festivalmanager/location/LocationManagementUnitTest.java
+++ src/test/java/festivalmanager/location/LocationManagementUnitTest.java
@@ -5,12 +5,18 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.salespointframework.core.Currencies.EURO;
 
+import java.util.Optional;
+
 import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Test;
 import org.salespointframework.useraccount.Password.UnencryptedPassword;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import org.salespointframework.useraccount.UserAccount;
 import org.salespointframework.useraccount.UserAccountManagement;
 
 public class LocationManagementUnitTest {
+		
 	
 }
