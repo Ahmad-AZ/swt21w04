4fc7cce57fa71c98c797ee63ed2c35b01894586d
Adrian <Adrian.Scholze@mailbox.tu-dresden.de>
2021-11-28 22:12:19 +0100

    add location tests

---
+++ src/test/java/festivalmanager/location/LocationManagementUnitTest.java
@@ -0,0 +1,16 @@
+package festivalmanager.location;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.salespointframework.core.Currencies.EURO;
+
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Test;
+import org.salespointframework.useraccount.Password.UnencryptedPassword;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManagement;
+
+public class LocationManagementUnitTest {
+	
+}
