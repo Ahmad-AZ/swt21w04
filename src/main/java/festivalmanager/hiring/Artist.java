035dd9332ac90a4593989f6c3255ecb79addfaa1
tutr464b <tuangiangtum@gmail.com>
2021-12-03 21:37:59 +0100

    add bookingArtist

--- src/main/java/festivalmanager/hiring/Artist.java
+++ src/main/java/festivalmanager/hiring/Artist.java
@@ -1,9 +1,12 @@
 package festivalmanager.hiring;
 
+import festivalmanager.location.Booking;
 import org.javamoney.moneta.Money;
+import org.salespointframework.time.Interval;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +19,10 @@ public class Artist {
 	private String name;
 	@Lob()
 	private Money price;
-	
+
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<BookingArtist> bookingArtists = new ArrayList<>();
+
 	@OneToMany(cascade = CascadeType.ALL)
 	private List<Show> shows;
 
@@ -52,4 +58,36 @@ public class Artist {
 	public void addShow(Show show){
 		shows.add(show);
 	}
+
+	public boolean addBooking(LocalDate startDate, LocalDate endDate) {
+		Interval festivalDateInterval = Interval.from(startDate.atStartOfDay()).to(endDate.atTime(23,59));
+		for (BookingArtist aBooking : bookingArtists) {
+			Interval aBookingDateInterval = Interval.from(aBooking.getStartDate().atStartOfDay()).to(aBooking.getEndDate().atTime(23, 59));
+			if (aBookingDateInterval.overlaps(festivalDateInterval)) {
+				System.out.println("Künstlerbelegt");
+				return false;
+			}
+		}
+		System.out.println("Künstler nicht belegt");
+		BookingArtist bookingArtist = new BookingArtist(startDate, endDate);
+		return bookingArtists.add(bookingArtist);
+	}
+
+	public boolean removeBooking(LocalDate startDate, LocalDate endDate) {
+		for (BookingArtist aBooking : bookingArtists) {
+			if (aBooking.getStartDate().equals(startDate) && aBooking.getEndDate().equals(endDate)) {
+				return bookingArtists.remove(aBooking);
+			}
+		}
+		return false;
+	}
+
+	public Iterable<BookingArtist> getBookingArtist() {
+		return bookingArtists;
+	}
+
+	public boolean hasBookingArtist() {
+		return !(bookingArtists.isEmpty());
+	}
+
 }
