bea15f54edf8b7a0da1ae6d1143b451a6c7f0197
tutr464b <tuangiangtum@gmail.com>
2021-12-03 13:38:59 +0100

    initialize show to artist

--- src/main/java/festivalmanager/hiring/ArtistDataInitializer.java
+++ src/main/java/festivalmanager/hiring/ArtistDataInitializer.java
@@ -16,8 +16,22 @@ public class ArtistDataInitializer implements DataInitializer {
 	public ArtistDataInitializer(ArtistRepository artists){
 		this.artists = artists;
 		Artist jackson = new Artist("Michael Jackson", Money.of(99.9, EURO));
+		Show criminal = new Show("Smooth criminal");
 		Artist shawn = new Artist("Shawn Mendes", Money.of(74.9, EURO));
 		Artist camila = new Artist("Camila Cabello", Money.of(83, EURO));
+
+		Show bad = new Show("bad");
+		Show senorita = new Show("senorita");
+		Show treat = new Show("treat you better");
+		Show havana = new Show("havana");
+		Show never = new Show("never be the same");
+		jackson.addShow(criminal);
+		jackson.addShow(bad);
+		shawn.addShow(senorita);
+		shawn.addShow(treat);
+		camila.addShow(havana);
+		camila.addShow(never);
+
 		artists.save(jackson);
 		artists.save(camila);
 		artists.save(shawn);
