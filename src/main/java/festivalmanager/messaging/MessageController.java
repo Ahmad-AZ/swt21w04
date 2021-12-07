6eeb7f2b02ac15b63a00573d3b9f743ee038f298
georgm.kunze@gmail.com <georgm.kunze@gmail.com>
2021-12-07 13:33:35 +0100

    added global userId model attribute, message controller functionality and empty message page

--- src/main/java/festivalmanager/messaging/MessageController.java
+++ src/main/java/festivalmanager/messaging/MessageController.java
@@ -1,9 +1,11 @@
 package festivalmanager.messaging;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MessageController {
@@ -15,7 +17,23 @@ public class MessageController {
 	}
 
 	@GetMapping("/messages/{receiverId}")
-	public String getMessageView(@PathVariable("receiverId") long receiverId) {
-		return "index.html";
+	public String getMessageView(@PathVariable("receiverId") long receiverId, Model model) {
+		model.addAttribute("messages", messageManagement.findByReceiverId(receiverId));
+
+		return "messages.html";
+	}
+
+	@GetMapping("/messages/{receiverId}/send")
+	public String getSendMessageDialog(@PathVariable("receiverId") long receiverId, Model model) {
+		model.addAttribute("messages", messageManagement.findByReceiverId(receiverId));
+		model.addAttribute("dialog", "send_message");
+
+		return "messages.html";
+	}
+
+	@PostMapping("/messages/{senderId}/send")
+	public String sendMessage(@PathVariable("senderId") long senderId, SendMessageForm form) {
+		messageManagement.sendMessage(form);
+		return "redirect:/messages/" + senderId;
 	}
 }
