ced844518661b25444adc9b7f7c69978901e385d
Jaaaaaan <jan.biedermann@mailbox.tu-dresden,de>
2021-12-12 22:35:37 +0100

    catering sales

--- src/main/java/festivalmanager/catering/CateringController.java
+++ src/main/java/festivalmanager/catering/CateringController.java
@@ -1,16 +1,22 @@
 package festivalmanager.catering;
 
+import org.salespointframework.order.Cart;
+import org.salespointframework.quantity.Quantity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ModelAttribute;
 import festivalmanager.festival.*;
 import festivalmanager.utils.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * @author Robert Menzel
  */
 @Controller
+@SessionAttributes("cart")
 public class CateringController {
 	private Festival currentFestival;
 	private UtilsManagement utilsManagement;
@@ -33,8 +39,13 @@ public class CateringController {
 		this.sales = sales;
 	}
 
+	@ModelAttribute("cart")
+	Cart initializeCart() {
+		return new Cart();
+	}
+
 	@GetMapping("/catering")
-	String sales(Model model) {
+	String sales(Model model, @ModelAttribute Cart cart) {
 		currentFestival = festivalManagement.findById(utilsManagement.getCurrentFestivalId()).get();
 		model.addAttribute("stock", stock.findByFestivalId(currentFestival.getId()));
 		model.addAttribute("productcatalog", catalog.findAll());
@@ -42,6 +53,15 @@ public class CateringController {
 		model.addAttribute("productid", null);
 		utilsManagement.setCurrentPageLowerHeader("cateringSales");
 		utilsManagement.prepareModel(model);
+		model.addAttribute("cart", cart);
 		return "catering";
 	}
+
+	//Daraus PostMapping methode bauen
+	/*
+	String addItem (Model mode, Hier ReqeustParam für item übergeben, Hier ReqeustParam für amount übergeben, @ModelAttribute Cart cart) {
+
+		cart.addOrUpdateItem(item, Quantity.of(amount));
+	}
+	*/
 }
