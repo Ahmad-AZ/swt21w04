2ab6a92b401e1456bf97d8b11c08a67b0bbcec35
georgm.kunze@gmail.com <georgm.kunze@gmail.com>
2022-01-09 16:52:28 +0100

    integrated group and global messages in the messaging controller

--- src/main/java/festivalmanager/messaging/SendGroupMessageForm.java
+++ src/main/java/festivalmanager/messaging/SendGroupMessageForm.java
@@ -2,32 +2,17 @@ package festivalmanager.messaging;
 
 import javax.validation.constraints.NotEmpty;
 
-public class SendGroupMessageForm {
-	@NotEmpty(message = "{SendGroupMessageForm.senderId.NotEmpty}")
-	private final long senderId;
-
+public class SendGroupMessageForm extends SendMessageForm {
 	@NotEmpty(message = "{SendGroupMessageForm.receiverFestivalId.NotEmpty}")
 	private final long receiverFestivalId;
 
 	@NotEmpty(message = "{SendGroupMessageForm.receiverGroup.NotEmpty}")
 	private final String receiverGroup;
 
-	@NotEmpty(message = "{SendGroupMessageForm.title.NotEmpty}")
-	private final String title;
-
-	@NotEmpty(message = "{SendGroupMessageForm.content.NotEmpty}")
-	private final String content;
-
 	public SendGroupMessageForm(long senderId, long receiverFestivalId, String receiverGroup, String title, String content) {
-		this.senderId = senderId;
+		super(senderId, title, content);
 		this.receiverFestivalId = receiverFestivalId;
 		this.receiverGroup = receiverGroup;
-		this.title = title;
-		this.content = content;
-	}
-
-	public long getSenderId() {
-		return senderId;
 	}
 
 	public long getReceiverFestivalId() {
@@ -36,13 +21,5 @@ public class SendGroupMessageForm {
 
 	public String getReceiverGroup() {
 		return receiverGroup;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getContent() {
-		return content;
 	}
 }
