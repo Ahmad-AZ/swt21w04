8e3dbc9b8125a524f468cc845918758ca8bed7e6
ahmad <ahmadabuzahra778@yahoo.com>
2022-01-15 17:01:09 +0100

    added javadoc for TicketShop package

--- src/main/java/festivalmanager/ticketShop/TicketRepository.java
+++ src/main/java/festivalmanager/ticketShop/TicketRepository.java
@@ -1,6 +1,7 @@
 package festivalmanager.ticketShop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
@@ -8,11 +9,25 @@ import java.util.List;
 import java.util.UUID;
 
 
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+/**
+ * A repository interface to manage {@link Ticket} instances.
+ *
+ * @author Ahmad Abu Zahra
+ */
 
+@Repository
+public interface TicketRepository extends CrudRepository<Ticket, UUID> {
+
+	/**
+	 * Re-declared {@link JpaRepository#findAll()} to return a {@link List} instead of {@link Iterable}.
+	 */
 	@Override
 	List<Ticket> findAll();
+
+	/**
+	 * Created Query to select {@link Ticket} by festivalId
+	 * @param id
+	 */
 	@Query("select t from Ticket t where t.festivalId = ?1")
 	Ticket findAllByFestivalId(long id);
 }
